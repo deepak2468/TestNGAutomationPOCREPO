c49208e574d5f05137a1c8ecc665a51e4edd9590
Deepak Arora <Deepak_Arora01@ad.infosys.com>
2018-05-30 12:10:02 +0530

    commiting the code changes made

---
+++ TestNGAutomationPOC/src/TestNGPackage/DeclareConstants.java
@@ -0,0 +1,23 @@
+package TestNGPackage;
+
+
+public final class DeclareConstants {
+	public static final String  DRIVER_PATH ="D:/chromedriver.exe";
+	public static final String  CHROME_DRIVER = "webdriver.chrome.driver";
+    public static final String  WEBSITE_URL = "http://adactin.com/HotelApp/";
+    public static final String LOGIN_PAGE_TITLE = "AdactIn.com - Hotel Reservation System";
+    public static final String SEARCH_HOTEL_PAGE_TITLE="AdactIn.com - Search Hotel";
+    public static final String LOCATION="Sydney";
+    public static final String HOTEL_NAME="Hotel Creek";
+    public static final String ROOM_TYPE="Standard";
+    public static final String CHILD_ROOM_TYPE="1 - One";
+    public static final String BOOKING_CONFIRMATION_PAGE_TITLE="AdactIn.com - Book A Hotel";
+    public static final String BOOKING_HOLDER_FIRST_PAGE="Deepak";
+    public static final String BOOKING_HOLDER_LAST_PAGE="Arora";
+    public static final String BOOKING_HOLDER_ADDRESS="H No 100 A/City";
+    public static final String CC_NUM="1234567891234567";
+    public static final String CC_TYPE="VISA";
+    public static final String MONTH="June";
+    public static final String YEAR="2022";
+    public static final String CC_CVV="1234";
+}
--- TestNGAutomationPOC/src/TestNGPackage/TestAutomationNGFile.java
+++ TestNGAutomationPOC/src/TestNGPackage/TestAutomationNGFile.java
@@ -6,25 +6,28 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+
 
 @Test
 public class TestAutomationNGFile {
        
   public WebDriver driver; 
-  public String driverPath = "D:/chromedriver.exe";
+  public String driverPath = DeclareConstants.DRIVER_PATH;
 
   /**
    * 
    */
   @BeforeTest
   public void launchBrowser() {
-         System.setProperty("webdriver.chrome.driver", driverPath);  
+         System.setProperty(DeclareConstants.CHROME_DRIVER, driverPath); 
          driver = new ChromeDriver();
-         driver.navigate().to("\r\n" + "http://adactin.com/HotelApp/");
+         driver.navigate().to("\r\n" + DeclareConstants.WEBSITE_URL);
   }
   
   /**
@@ -33,7 +36,10 @@ public class TestAutomationNGFile {
    */
   @Test(priority=0)
   public void validateLogin()  throws InterruptedException {
-              driver.findElement(By.xpath("//*[@id=\'username\']")).sendKeys("deepak123");
+	  
+	  		  // To verify title of login page
+	  		  Assert.assertEquals(DeclareConstants.LOGIN_PAGE_TITLE, driver.getTitle());
+	  		  driver.findElement(By.xpath("//*[@id=\'username\']")).sendKeys("deepak123");
               driver.findElement(By.xpath("//*[@id=\'password\']")).sendKeys("Infosys123");
               driver.findElement(By.xpath("//*[@id=\'login\']")).click();
   }
@@ -44,28 +50,54 @@ public class TestAutomationNGFile {
    */
   @Test(priority=1)
   public void searchHotel() throws InterruptedException {
+	  		
+	     	  // To verify title of search hotel page
+              Assert.assertEquals(DeclareConstants.SEARCH_HOTEL_PAGE_TITLE, driver.getTitle());
               
-              TimeUnit.MILLISECONDS.sleep(2000);
+              //To verify the login user
+              WebElement userNameElement = driver.findElement(By.xpath("//*[@id='username_show']"));
+              String userName = userNameElement.getAttribute("value");
+              Assert.assertEquals("Hello deepak123!", userName);
+	  		  
+	  		  TimeUnit.MILLISECONDS.sleep(2000);
               Select location= new Select(driver.findElement(By.xpath("//*[@id='location']")));
-              location.selectByVisibleText("Sydney");
+              location.selectByVisibleText(DeclareConstants.LOCATION);
               
               TimeUnit.MILLISECONDS.sleep(2000);
               
               Select hotels= new Select(driver.findElement(By.xpath("//*[@id='hotels']")));
-              hotels.selectByVisibleText("Hotel Creek");
+              hotels.selectByVisibleText(DeclareConstants.HOTEL_NAME);
               
               TimeUnit.MILLISECONDS.sleep(2000);
               
               Select roomType= new Select(driver.findElement(By.xpath("//*[@id=\'room_type\']")));
-              roomType.selectByVisibleText("Standard");
+              roomType.selectByVisibleText(DeclareConstants.ROOM_TYPE);
               
               TimeUnit.MILLISECONDS.sleep(2000);
               
               Select childRoom= new Select(driver.findElement(By.xpath("//*[@id=\'child_room\']")));
-              childRoom.selectByVisibleText("1 - One");
+              childRoom.selectByVisibleText(DeclareConstants.CHILD_ROOM_TYPE);
               
               TimeUnit.MILLISECONDS.sleep(2000);
               
+              //Click on reset button
+              driver.findElement(By.xpath("//*[@id=\'Reset\']")).click();
+              
+              //To verify that select boxes resets to default values
+              Assert.assertEquals("- Select Location -", location.getFirstSelectedOption().getText());
+              Assert.assertEquals("- Select Hotel -", hotels.getFirstSelectedOption().getText());
+              Assert.assertEquals("- Select Room Type -", roomType.getFirstSelectedOption().getText());
+              Assert.assertEquals("- Select Children per Room -", childRoom.getFirstSelectedOption().getText());
+              
+              TimeUnit.MILLISECONDS.sleep(2000);
+              location.selectByVisibleText(DeclareConstants.LOCATION);
+              TimeUnit.MILLISECONDS.sleep(2000);
+              hotels.selectByVisibleText(DeclareConstants.HOTEL_NAME);
+              TimeUnit.MILLISECONDS.sleep(2000);
+              roomType.selectByVisibleText(DeclareConstants.ROOM_TYPE);
+              TimeUnit.MILLISECONDS.sleep(2000);
+              childRoom.selectByVisibleText(DeclareConstants.CHILD_ROOM_TYPE);
+              TimeUnit.MILLISECONDS.sleep(2000);
               
               driver.findElement(By.xpath("//*[@id=\'Submit\']")).click();
               TimeUnit.MILLISECONDS.sleep(1000);
@@ -89,36 +121,40 @@ public class TestAutomationNGFile {
    * @throws InterruptedException
    */
   @Test(priority=3)
-  public void bookingConfirmation() throws InterruptedException {       
-              driver.findElement(By.xpath("//*[@id=\'first_name\']")).sendKeys("Deepak");
+  public void bookingConfirmation() throws InterruptedException {  
+	  
+ 	  		  // To verify title of book hotel page
+      		  Assert.assertEquals(DeclareConstants.BOOKING_CONFIRMATION_PAGE_TITLE, driver.getTitle());
+      
+              driver.findElement(By.xpath("//*[@id=\'first_name\']")).sendKeys(DeclareConstants.BOOKING_HOLDER_FIRST_PAGE);
               TimeUnit.MILLISECONDS.sleep(1000);
               
               
-              driver.findElement(By.xpath("//*[@id=\'last_name\']")).sendKeys("Arora");
+              driver.findElement(By.xpath("//*[@id=\'last_name\']")).sendKeys(DeclareConstants.BOOKING_HOLDER_LAST_PAGE);
               TimeUnit.MILLISECONDS.sleep(1000);
               
               
-              driver.findElement(By.xpath("//*[@id=\'address\']")).sendKeys("H No 100 A/City");
+              driver.findElement(By.xpath("//*[@id=\'address\']")).sendKeys(DeclareConstants.BOOKING_HOLDER_ADDRESS);
               TimeUnit.MILLISECONDS.sleep(1000);
               
               
-              driver.findElement(By.xpath("//*[@id=\'cc_num\']")).sendKeys("1234567891234567");
+              driver.findElement(By.xpath("//*[@id=\'cc_num\']")).sendKeys(DeclareConstants.CC_NUM);
               TimeUnit.MILLISECONDS.sleep(1000);
               
               
               Select ccType= new Select(driver.findElement(By.xpath("//*[@id=\'cc_type\']")));
-              ccType.selectByVisibleText("VISA");
+              ccType.selectByVisibleText(DeclareConstants.CC_TYPE);
               TimeUnit.MILLISECONDS.sleep(1000);
               
               Select ccExpMonth= new Select(driver.findElement(By.xpath("//*[@id=\'cc_exp_month\']")));
-              ccExpMonth.selectByVisibleText("June"); 
+              ccExpMonth.selectByVisibleText(DeclareConstants.MONTH); 
               TimeUnit.MILLISECONDS.sleep(1000);
               
               Select ccExpYear= new Select(driver.findElement(By.xpath("//*[@id=\'cc_exp_year\']")));
-              ccExpYear.selectByVisibleText("2022"); 
+              ccExpYear.selectByVisibleText(DeclareConstants.YEAR); 
               TimeUnit.MILLISECONDS.sleep(1000);
               
-              driver.findElement(By.xpath("//*[@id=\'cc_cvv\']")).sendKeys("1234");   
+              driver.findElement(By.xpath("//*[@id=\'cc_cvv\']")).sendKeys(DeclareConstants.CC_CVV);   
               TimeUnit.MILLISECONDS.sleep(1000);
               
               driver.findElement(By.xpath("//*[@id=\'book_now\']")).click();
